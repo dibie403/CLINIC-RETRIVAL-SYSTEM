d4821b715429e42f118ff85ddc81c34d6264a54c
dibie403 <152690737+dibie403@users.noreply.github.com>
2025-01-10 16:55:36 +0100

    change

--- main.py
+++ main.py
@@ -29,6 +29,16 @@ posts = [
      'content': 'This is my very second post',
      'date': 'Oct 9,2029'
      },
+{'author': 'Mike Ben',
+     'title': 'Second Blog Post',
+     'content': 'This is my very second post',
+     'date': 'Oct 9,2029'
+     },
+{'author': 'Mike Ben',
+     'title': 'Second Blog Post',
+     'content': 'This is my very second post',
+     'date': 'Oct 9,2029'
+     },
     
 ]
 
